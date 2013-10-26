9c8d001f5421478c68f5ddc109c3f6b21e08b6f0
zdanowiczkonrad <zdanowiczkonrad@gmail.com>
2013-10-26 17:55:53 +0200

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ BrailleRecognition
 ==================
 
 Braille Alphabet Recognition using Multi-Layer Pereceptron Neural Networks and back-propagation learning algorithm
+
+Commit test
---
+++ braille-recognition/src/main/java/com/kzdanowi/studies/softcomputing/App.java
@@ -0,0 +1,13 @@
+package com.kzdanowi.studies.softcomputing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
